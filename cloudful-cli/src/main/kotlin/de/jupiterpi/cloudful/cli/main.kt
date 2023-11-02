6e6c95fb179f1647da96a298a1a2b0e039f04c22
JupiterPi <jupiterpi@outlook.de>
2023-11-02 16:25:50 +0100

    primitive init command

--- cloudful-cli/src/main/kotlin/de/jupiterpi/cloudful/cli/main.kt
+++ cloudful-cli/src/main/kotlin/de/jupiterpi/cloudful/cli/main.kt
@@ -2,26 +2,35 @@ package de.jupiterpi.cloudful.cli
 
 import picocli.CommandLine
 import picocli.CommandLine.Command
+import picocli.CommandLine.Parameters
 import java.awt.Desktop
 import java.net.URI
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val exitCode = CommandLine(CloudfulCommand()).execute(*args)
+    val exitCode = CommandLine(CloudfulCommand())
+        .setExecutionExceptionHandler { e, _, _ ->
+            if (e is DisplayException) println("Error: ${e.message}") else e.printStackTrace()
+            1
+        }
+        .execute(*args)
     exitProcess(exitCode)
 }
+
+class DisplayException(msg: String) : Exception(msg)
 
 @Command(name = "cloudful", subcommands = [
     CommandLine.HelpCommand::class,
     SyncCommand::class,
     OpenCommand::class,
+    InitCommand::class,
 ])
 class CloudfulCommand
 
 @Command(name = "sync", description = ["Syncs the current repository."])
 class SyncCommand : Runnable {
     override fun run() {
-        val repository = readRepository()
+        val repository = readRepository() ?: throw DisplayException("No repository found!")
         println("Syncing repository ${repository.repositoryPath} ...\n")
         sync(repository)
     }
@@ -30,7 +39,7 @@ class SyncCommand : Runnable {
 @Command(name = "open", description = ["Opens the current directory in the Cloud Console."])
 class OpenCommand : Runnable {
     override fun run() {
-        val repository = readRepository()
+        val repository = readRepository() ?: throw DisplayException("No repository found!")
         val url = "https://console.cloud.google.com/storage/browser/$BUCKET$REPOSITORIES_ROOT/${repository.repositoryPath}/${repository.openPath}"
 
         // see https://stackoverflow.com/a/5226244/13164753
@@ -38,4 +47,12 @@ class OpenCommand : Runnable {
             Desktop.getDesktop().browse(URI(url))
         }
     }
+}
+
+@Command(name = "init", description = ["Initialize the current directory as a new repository."])
+class InitCommand : Runnable {
+    @Parameters(index = "0", paramLabel = "<repo path>")
+    lateinit var repositoryPath: String
+
+    override fun run() = createRepository(repositoryPath)
 }
--- cloudful-cli/src/main/kotlin/de/jupiterpi/cloudful/cli/repositories.kt
+++ cloudful-cli/src/main/kotlin/de/jupiterpi/cloudful/cli/repositories.kt
@@ -7,7 +7,8 @@ fun sync(repository: Repository) {
     val excludePattern = repository.excludePaths
         .map { it.replace("\\", "\\\\").replace(".", "\\.").replace("*", ".*") }
         .joinToString(separator = "|") { "(^$it$)" }
-    val cmd = "gsutil -m rsync -r -d -x \"$excludePattern\" ${repository.root} gs://$BUCKET$REPOSITORIES_ROOT/${repository.repositoryPath}"
+    val excludeStr = if (excludePattern.isBlank()) "" else "-x \"$excludePattern\""
+    val cmd = "gsutil -m rsync -r -d $excludeStr ${repository.root} gs://$BUCKET$REPOSITORIES_ROOT/${repository.repositoryPath}"
     println("> $cmd")
     Runtime.getRuntime().exec("cmd.exe /c $cmd").let {
         it.inputStream.transferTo(System.out)
@@ -15,11 +16,16 @@ fun sync(repository: Repository) {
     }
 }
 
+fun createRepository(repositoryPath: String) {
+    if (readRepository() != null) throw DisplayException("There's already a repository here!")
+    Path(".cloudful").createFile().writeText("> $repositoryPath\n\n# Exclude files here...")
+}
+
 @OptIn(ExperimentalPathApi::class)
-fun readRepository(): Repository {
+fun readRepository(): Repository? {
     var repositoryRoot = Path("").absolute()
     while (!(repositoryRoot / ".cloudful").exists()) {
-        repositoryRoot = repositoryRoot.parent
+        repositoryRoot = repositoryRoot.parent ?: return null
     }
 
     val configurationLines = (repositoryRoot / ".cloudful").readLines()
@@ -28,13 +34,13 @@ fun readRepository(): Repository {
     val openPath = repositoryRoot.relativize(Path("").absolute()).toString().replace("\\", "/")
 
     val excludePaths = configurationLines
-        .filter { it.isNotEmpty() && !it.startsWith(">") }
+        .filter { it.isNotEmpty() && !it.startsWith("#") && !it.startsWith(">")}
         .map { it.trim() }
         .map { if (!it.contains(Regex("(!|\\*)")) && (repositoryRoot / it).isDirectory()) "$it\\*" else it }
         .toMutableList()
 
     repositoryRoot.walk().filter { it.fileName.toString() == ".cloudignore" }
-        .forEach { file -> excludePaths += file.readLines().map { "${repositoryRoot.relativize(file.parent)}\\${it.trim()}" } }
+        .forEach { file -> excludePaths += file.readLines().filter { !it.startsWith("#") }.map { "${repositoryRoot.relativize(file.parent)}\\${it.trim()}" } }
 
     return Repository(repositoryRoot, repositoryPath, excludePaths, openPath)
 }
